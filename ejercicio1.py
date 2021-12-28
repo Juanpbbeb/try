87a632004285d4cfc3865a240bc5a4c018870755
Juan <chaskitaxd12@gmail.com>
2021-12-28 10:49:07 -0500

    repositorio

---
+++ ejercicio1.py
@@ -0,0 +1,9 @@
+usuario=input("Ingresa el usuario:")
+if usuario =="maria":
+    contraseña =input("ingresa contraseña: ")
+    if contraseña == "juan123":
+            print("los datos son correctos")
+    else:
+       print("la contraseña es incorrecta ")
+else:
+       print("el usuario es incorrecto")
